ca3efc6e2c4d985d754a36be5f890fa10730e078
Roland Werner <roland.werner@muenchen.de>
2018-03-12 12:35:14 +0100

    Turned SecurityInterceptor into a Spring class and general cleanup

--- webapp/src/main/java/org/example/Application.java
+++ webapp/src/main/java/org/example/Application.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -10,12 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableOAuth2Sso
 public class Application  extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private SecurityInterceptor securityInterceptor;
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
         @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new SecurityInterceptor());
+        registry.addInterceptor(securityInterceptor);
     }
 }
